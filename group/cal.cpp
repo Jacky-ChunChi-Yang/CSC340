c1310dea7f87c6ec041328028da98d70af0bbe35
Jacky-ChunChi-Yang <77chunchi@gmail.com>
2024-12-03 14:21:43 -0800

    new
    please do something group

--- group/cal.cpp
+++ group/cal.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <string>
 #include <memory>
-#include <vector>
 #include <iomanip>
+#include <regex>
 #include <ctime>
 using namespace std;
 
@@ -35,7 +35,7 @@ public:
 class MeetingEvent : public Event {
     string organizer;
 public:
-    MeetingEvent(string n, string d, string t, string o) 
+    MeetingEvent(string n, string d, string t, string o)
         : Event(n, d, t), organizer(o) {}
     void display() const override {
         cout << "Meeting: " << name << " on " << date << " at " << time
@@ -47,7 +47,7 @@ public:
 class PersonalEvent : public Event {
     string location;
 public:
-    PersonalEvent(string n, string d, string t, string loc) 
+    PersonalEvent(string n, string d, string t, string loc)
         : Event(n, d, t), location(loc) {}
     void display() const override {
         cout << "Personal Event: " << name << " on " << date << " at " << time
@@ -103,41 +103,97 @@ public:
     }
 };
 
+// Month node
+class MonthNode {
+public:
+    string monthName;
+    Day* days[31]; // Max 31 days in a month
+    MonthNode* next;
+
+    MonthNode(const string& name) : monthName(name), next(nullptr) {
+        for (int i = 0; i < 31; ++i) days[i] = new Day();
+    }
+
+    ~MonthNode() {
+        for (int i = 0; i < 31; ++i) delete days[i];
+    }
+};
+
 // Calendar class
 class Calendar {
-    vector<vector<Day> > months; // 12 months with 31 days each
+    MonthNode* head;
     int year;
+
+    // Helper to get month name
+    string getMonthName(int month) const {
+        static const string monthNames[] = {
+            "January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"
+        };
+        return monthNames[month];
+    }
+
+    MonthNode* getMonthNode(int month) const {
+        MonthNode* current = head;
+        for (int i = 0; i < month; ++i) current = current->next;
+        return current;
+    }
+
 public:
-    Calendar(int y) : year(y), months(12, vector<Day>(31)) {}
+    Calendar(int y) : year(y), head(nullptr) {
+        MonthNode* tail = nullptr;
+        for (int i = 0; i < 12; ++i) {
+            MonthNode* newNode = new MonthNode(getMonthName(i));
+            if (!head) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail->next = newNode;
+                tail = newNode;
+            }
+        }
+    }
 
     void addEvent(shared_ptr<Event> event) {
         string date = event->getDate();
         int month = stoi(date.substr(5, 2)) - 1;
         int day = stoi(date.substr(8, 2)) - 1;
-        months[month][day].addEvent(event);
+        getMonthNode(month)->days[day]->addEvent(event);
     }
 
     void searchEvent(const string& date) const {
         int month = stoi(date.substr(5, 2)) - 1;
         int day = stoi(date.substr(8, 2)) - 1;
+        MonthNode* monthNode = getMonthNode(month);
         cout << "Events on " << date << ":" << endl;
-        months[month][day].displayEvents();
+        monthNode->days[day]->displayEvents();
     }
 
     void displayMonth(int month) const {
-        cout << "Calendar for " << year << "-" << setw(2) << setfill('0') << month + 1 << ":\n";
+        if (month < 1 || month > 12) {
+            cout << "Invalid month! Please enter a month between 1 and 12." << endl;
+            return;
+        }
+        
+        MonthNode* monthNode = getMonthNode(month - 1);
+        cout << "Calendar for " << year << " - " << monthNode->monthName << ":\n";
         cout << "Su  Mo  Tu  We  Th  Fr  Sa\n";
+
+        // Calculate starting weekday
         time_t t = time(nullptr);
         tm timeInfo = *localtime(&t);
         timeInfo.tm_year = year - 1900;
-        timeInfo.tm_mon = month;
+        timeInfo.tm_mon = month - 1;
         timeInfo.tm_mday = 1;
         mktime(&timeInfo);
         int startDay = timeInfo.tm_wday;
-        int daysInMonth = (month == 1 && (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))) ? 29 : (month == 0 || month == 2 || month == 4 || month == 6 || month == 7 || month == 9 || month == 11) ? 31 : 30;
-        for (int i = 0; i < startDay; i++) cout << "   ";
+
+        int daysInMonth = (month == 2 && (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))) ? 29 :
+                          (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) ? 31 : 30;
+
+        for (int i = 0; i < startDay; i++) cout << "    ";
         for (int day = 1; day <= daysInMonth; day++) {
-            if (months[month][day - 1].hasEvents()) {
+            if (monthNode->days[day - 1]->hasEvents()) {
                 cout << setw(2) << setfill('0') << day << "* ";
             } else {
                 cout << setw(2) << setfill('0') << day << "  ";
@@ -146,12 +202,23 @@ public:
         }
         cout << endl;
     }
+
+    ~Calendar() {
+        while (head) {
+            MonthNode* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
 };
 
 // Main function
 int main() {
-    int year = 2024;
+    int year;
+    cout << "Enter the year for your calendar: ";
+    cin >> year;
     Calendar calendar(year);
+    
     while (true) {
         cout << "Choose an action: (1) Add Event (2) Search Event (3) Display Month (4) Exit\n";
         int action;
@@ -179,7 +246,7 @@ int main() {
                 cin.ignore();
                 getline(cin, extra);
                 calendar.addEvent(make_shared<PersonalEvent>(name, date, time, extra));
-            }
+            } 
             break;
         }
         case SEARCH_EVENT: {
@@ -193,7 +260,7 @@ int main() {
             int month;
             cout << "Enter month (1-12): ";
             cin >> month;
-            calendar.displayMonth(month - 1);
+            calendar.displayMonth(month);
             break;
         }
         default:
